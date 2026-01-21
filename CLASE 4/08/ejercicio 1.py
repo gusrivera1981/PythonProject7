042cb95149707f87d21023abe68157c0cb280e04
Gustavo Rivera <gustavorivera16933186@gmail.com>
2026-01-21 15:36:10 -0500

    🗑️ Eliminar carpetas inecesarias del repositorio

--- CLASE 4/08/ejercicio 1.py
+++
@@ -1,7 +0,0 @@
-nombre = input("ingrese su nombre: ")
-año_nacimiento = int(input("ingrese su año de nacimiento: "))
-año_actual = int(input("ingrese el año actual: " ))
-
-edad_aproximada = año_actual - año_nacimiento
-
-print(f"hola,{nombre}.tienes aproximadamente,{edad_aproximada}años  ")
